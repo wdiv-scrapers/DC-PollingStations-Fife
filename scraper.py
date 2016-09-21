b6c81ab598ac6320e20169a19f50e0322d851cf6
chris48s <c.mshaw@yahoo.co.uk>
2016-09-21 20:46:14 +0100

    use RandomIdGeoJSONScraper so we generate a consistent content_hash

--- scraper.py
+++ scraper.py
@@ -1,26 +1,12 @@
-from dc_base_scrapers.xml_scraper import Wfs2Scraper
+from dc_base_scrapers.geojson_scraper import RandomIdGeoJSONScraper
 
 
-stations_url = "http://arcgisweb.fife.gov.uk/geoserver/fife/wfs?service=WFS&version=1.3.0&request=GetFeature&typeNames=fife%3APOLLING_PLACES&srsName=EPSG%3A4326"
-stations_fields = {
-    '{http://arcgisweb.fife.gov.uk/geoserver}CODE': 'CODE',
-    '{http://arcgisweb.fife.gov.uk/geoserver}NAME': 'NAME',
-    '{http://arcgisweb.fife.gov.uk/geoserver}POLLING_PLACE': 'POLLING_PLACE',
-    '{http://arcgisweb.fife.gov.uk/geoserver}OBJECTID': 'OBJECTID',
-}
-
-districts_url = "http://arcgisweb.fife.gov.uk/geoserver/fife/wfs?service=WFS&version=1.3.0&request=GetFeature&typeNames=fife%3APOLLING_DISTRICT&srsName=EPSG%3A4326"
-districts_fields = {
-    '{http://arcgisweb.fife.gov.uk/geoserver}CODE': 'CODE',
-    '{http://arcgisweb.fife.gov.uk/geoserver}NAME': 'NAME',
-    '{http://arcgisweb.fife.gov.uk/geoserver}POLLING_PLACE': 'POLLING_PLACE',
-    '{http://arcgisweb.fife.gov.uk/geoserver}OBJECTID': 'OBJECTID',
-}
-
+stations_url = "http://arcgisweb.fife.gov.uk/geoserver/fife/wfs?service=WFS&version=1.3.0&request=GetFeature&typeNames=fife%3APOLLING_PLACES&outputFormat=json&srsName=EPSG%3A4326&sortBy=OBJECTID"
+districts_url = "http://arcgisweb.fife.gov.uk/geoserver/fife/wfs?service=WFS&version=1.3.0&request=GetFeature&typeNames=fife%3APOLLING_DISTRICT&outputFormat=json&srsName=EPSG%3A4326&sortBy=OBJECTID"
 council_id = 'S12000015'
 
 
-stations_scraper = Wfs2Scraper(stations_url, council_id, 'stations', stations_fields, 'OBJECTID')
+stations_scraper = RandomIdGeoJSONScraper(stations_url, council_id, 'utf-8', 'stations')
 stations_scraper.scrape()
-districts_scraper = Wfs2Scraper(districts_url, council_id, 'districts', districts_fields, 'OBJECTID')
+districts_scraper = RandomIdGeoJSONScraper(districts_url, council_id, 'utf-8', 'districts')
 districts_scraper.scrape()
