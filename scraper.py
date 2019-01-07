5fd65b55e41e4c00976ff81d501ef85051628d38
chris48s <chris.shaw480@gmail.com>
2019-01-07 10:54:02 +0000

    update GSS code

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from dc_base_scrapers.geojson_scraper import RandomIdGeoJSONScraper
 
 stations_url = "http://arcgisweb.fife.gov.uk/geoserver/fife/wfs?service=WFS&version=1.3.0&request=GetFeature&typeNames=fife%3APOLLING_PLACES&outputFormat=json&srsName=EPSG%3A4326&sortBy=OBJECTID"
 districts_url = "http://arcgisweb.fife.gov.uk/geoserver/fife/wfs?service=WFS&version=1.3.0&request=GetFeature&typeNames=fife%3APOLLING_DISTRICT&outputFormat=json&srsName=EPSG%3A4326&sortBy=OBJECTID"
-council_id = 'S12000015'
+council_id = 'S12000047'
 
 
 stations_scraper = RandomIdGeoJSONScraper(stations_url, council_id, 'utf-8', 'stations')
